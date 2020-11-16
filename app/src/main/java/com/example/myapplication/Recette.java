089c9bd84811581fad7989e2d05f2886b6b66114
carolinelagarde <caro.lag@outlook.fr>
2020-11-16 15:40:57 +0100

    création des classes recettes et ingredient

---
+++ app/src/main/java/com/example/myapplication/Recette.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import java.util.Vector;
+
+public class Recette {
+
+    //titre
+    private String titre;
+
+    //ingrédients
+    private Vector<Ingredient> lesIngredients;
+
+    //les instructions
+    private String instructions;
+
+    //temps
+    private int tempsdecuisine;
+
+    /** creer une nouvelle recette
+     *
+     */
+
+    public Recette(String titre, int tempsdecuisine, Vector<Ingredient> lesIngredients, String instructions) {
+        this.titre=titre;
+        this.tempsdecuisine=tempsdecuisine;
+        this.lesIngredients=lesIngredients;
+        this.instructions=instructions;
+    }
+
+    //obtenir le titre de la recette
+    public String getTitre() { return titre; }
+
+    //obtenir les ingrédients avec ses quantités
+    public Vector<Ingredient> getIngredients() { return lesIngredients; }
+
+    //obtenir le temps de cuisine nécessaire
+    public int getTempsdecuisine() { return tempsdecuisine; }
+
+    //obtenir les instructions de cuisine
+    public String getInstructions() { return instructions; }
+}
